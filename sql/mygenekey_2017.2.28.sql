5a9b48706161876ba01853c054c2a94353df5baa
uoor <838199384@qq.com>
2017-02-28 13:46:50 +0800

    sql

---
+++ sql/mygenekey_2017.2.28.sql
@@ -0,0 +1,320 @@
+-- MySQL dump 10.13  Distrib 5.6.19, for linux-glibc2.5 (x86_64)
+--
+-- Host: localhost    Database: mygenekey
+-- ------------------------------------------------------
+-- Server version	5.5.53-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `adminuser`
+--
+
+DROP TABLE IF EXISTS `adminuser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `adminuser` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adminuser`
+--
+
+LOCK TABLES `adminuser` WRITE;
+/*!40000 ALTER TABLE `adminuser` DISABLE KEYS */;
+INSERT INTO `adminuser` VALUES (2,'admin','admin');
+/*!40000 ALTER TABLE `adminuser` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `barcode`
+--
+
+DROP TABLE IF EXISTS `barcode`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `barcode` (
+  `bcid` int(11) NOT NULL AUTO_INCREMENT,
+  `bar_code` varchar(255) DEFAULT NULL COMMENT '要确定好格式',
+  `status` int(11) DEFAULT '0' COMMENT '0 未激活;1 已激活 ',
+  PRIMARY KEY (`bcid`),
+  KEY `idx_barcode_bar_code` (`bar_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `barcode`
+--
+
+LOCK TABLES `barcode` WRITE;
+/*!40000 ALTER TABLE `barcode` DISABLE KEYS */;
+/*!40000 ALTER TABLE `barcode` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `cname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'美容系列');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categorysecond`
+--
+
+DROP TABLE IF EXISTS `categorysecond`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categorysecond` (
+  `csid` int(11) NOT NULL AUTO_INCREMENT,
+  `csname` varchar(255) DEFAULT NULL,
+  `cid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`csid`),
+  KEY `FK936FCAF21DB1FD15` (`cid`),
+  CONSTRAINT `FK936FCAF21DB1FD15` FOREIGN KEY (`cid`) REFERENCES `category` (`cid`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorysecond`
+--
+
+LOCK TABLES `categorysecond` WRITE;
+/*!40000 ALTER TABLE `categorysecond` DISABLE KEYS */;
+INSERT INTO `categorysecond` VALUES (1,'美容系列',1);
+/*!40000 ALTER TABLE `categorysecond` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `logisticscompany`
+--
+
+DROP TABLE IF EXISTS `logisticscompany`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `logisticscompany` (
+  `lcid` int(11) NOT NULL AUTO_INCREMENT,
+  `logistic_name` varchar(50) DEFAULT NULL COMMENT '物流公司名称',
+  PRIMARY KEY (`lcid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `logisticscompany`
+--
+
+LOCK TABLES `logisticscompany` WRITE;
+/*!40000 ALTER TABLE `logisticscompany` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logisticscompany` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orderitem`
+--
+
+DROP TABLE IF EXISTS `orderitem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orderitem` (
+  `itemid` int(11) NOT NULL AUTO_INCREMENT,
+  `count` int(11) DEFAULT NULL,
+  `subtotal` double DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `oid` int(11) DEFAULT NULL,
+  `bar_code` varchar(50) DEFAULT NULL COMMENT '要确定好格式',
+  PRIMARY KEY (`itemid`),
+  KEY `FKE8B2AB6166C01961` (`oid`),
+  KEY `FKE8B2AB6171DB7AE4` (`pid`),
+  KEY `FKE8B2AB6140ACF87A` (`oid`),
+  CONSTRAINT `FKE8B2AB6140ACF87A` FOREIGN KEY (`oid`) REFERENCES `orders` (`oid`),
+  CONSTRAINT `FKE8B2AB6171DB7AE4` FOREIGN KEY (`pid`) REFERENCES `product` (`pid`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orderitem`
+--
+
+LOCK TABLES `orderitem` WRITE;
+/*!40000 ALTER TABLE `orderitem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orderitem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `oid` int(11) NOT NULL AUTO_INCREMENT,
+  `total` double DEFAULT NULL,
+  `ordertime` datetime DEFAULT NULL COMMENT '订单生产时间',
+  `state` int(11) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `addr` varchar(50) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `accept_time` datetime DEFAULT NULL COMMENT '订单接受时间',
+  `refuse_time` datetime DEFAULT NULL COMMENT '订单拒绝时间',
+  `end_time` datetime DEFAULT NULL COMMENT '订单结束时间',
+  `supply_time` datetime DEFAULT NULL COMMENT '发货时订单间',
+  `back_time` datetime DEFAULT NULL COMMENT '订单退货时间',
+  `balance` int(11) DEFAULT NULL COMMENT '付款情况',
+  `payment_type` int(11) DEFAULT NULL COMMENT '付款方式',
+  `account_number` varchar(50) DEFAULT NULL COMMENT '转账流水号（唯一），防止退款用',
+  `pay_time` datetime DEFAULT NULL COMMENT '订单付款时间',
+  `lcid` int(11) DEFAULT NULL COMMENT '物流公司',
+  `waybill_numer` varchar(50) DEFAULT NULL COMMENT '运单编号',
+  PRIMARY KEY (`oid`),
+  KEY `FKC3DF62E5AA3D9C7` (`uid`),
+  KEY `FK_logisticscompany_orders` (`lcid`),
+  CONSTRAINT `FK_logisticscompany_orders` FOREIGN KEY (`lcid`) REFERENCES `logisticscompany` (`lcid`),
+  CONSTRAINT `FKC3DF62E5AA3D9C7` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=9004 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `pid` int(11) NOT NULL AUTO_INCREMENT,
+  `pname` varchar(255) DEFAULT NULL,
+  `market_price` double DEFAULT NULL,
+  `shop_price` double DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `pdesc` varchar(255) DEFAULT NULL,
+  `is_hot` int(11) DEFAULT NULL,
+  `pdate` datetime DEFAULT NULL,
+  `csid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`pid`),
+  KEY `FKED8DCCEFB9B74E02` (`csid`),
+  CONSTRAINT `FKED8DCCEFB9B74E02` FOREIGN KEY (`csid`) REFERENCES `categorysecond` (`csid`)
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `salivabox`
+--
+
+DROP TABLE IF EXISTS `salivabox`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `salivabox` (
+  `sbid` int(11) NOT NULL AUTO_INCREMENT,
+  `sex` varchar(20) DEFAULT NULL COMMENT '性别',
+  `bar_code` varchar(50) DEFAULT NULL COMMENT '要确定好格式',
+  `birthday` datetime DEFAULT NULL COMMENT '出生日期',
+  `dna_accredit` int(11) DEFAULT '0' COMMENT '0 不允许 1允许',
+  `uid` int(11) DEFAULT NULL COMMENT '使用的用户',
+  PRIMARY KEY (`sbid`),
+  KEY `FK_user_salivabox` (`uid`),
+  CONSTRAINT `FK_user_salivabox` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salivabox`
+--
+
+LOCK TABLES `salivabox` WRITE;
+/*!40000 ALTER TABLE `salivabox` DISABLE KEYS */;
+/*!40000 ALTER TABLE `salivabox` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `addr` varchar(255) DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  `code` varchar(64) DEFAULT NULL,
+  `bar_code` varchar(50) DEFAULT NULL COMMENT '要确定好格式',
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (7,'aaa','aaa','姜涛','aaa@shop.com','15726607618','北京市西三旗中腾建华3楼',1,NULL,NULL),(8,'bbb','bbb','张三','bbb@shop.com','18726607618','北京市西三旗中腾建华3楼',1,'',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-10 12:10:27
