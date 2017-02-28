de1cbdac09a0d79a20784fc2927b35431a766e5d
uoor <838199384@qq.com>
2017-02-28 15:59:00 +0800

    struts change

--- src/cn/mygenekey/salivaBox/action/SalivaBoxAction.java
+++ src/cn/mygenekey/salivaBox/action/SalivaBoxAction.java
@@ -2,14 +2,20 @@ package cn.mygenekey.salivaBox.action;
 
 import cn.mygenekey.base.BaseAction;
 import cn.mygenekey.salivaBox.vo.SalivaBox;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by mh on 17-2-28.
  */
+@Controller
+@Scope("prototype")
 public class SalivaBoxAction extends BaseAction<SalivaBox> {
 	/*跳转至绑定盒子
 	 */
 	public String bindBox(){
+		System.out.println("到这了吗？？");
+
 		return "bindBox";
 	}
 }
